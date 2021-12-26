513a06cacc15c180e0172dcd841d4bf5c997c578
hartha.aloufi <hartha.aloufi@gmail.com>
2021-12-26 19:16:19 +0200

    working on rtl

---
+++ data/headerNavLinks.js
@@ -0,0 +1,8 @@
+const headerNavLinks = [
+  { href: '/blog', title: 'Blog' },
+  { href: '/about', title: 'About' },
+  // { href: '/tags', title: 'Tags' },
+  // { href: '/projects', title: 'Projects' },
+]
+
+export default headerNavLinks
