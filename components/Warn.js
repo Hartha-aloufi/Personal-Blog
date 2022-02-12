c5a69ecdbd86af3e5f8e92c4c4fa1d114b0e498d
hartha-aloufi <hartha.aloufi@gmail.com>
2022-02-12 22:28:08 +0200

    Add warning to blog: latency and bandwidth

--- components/Warn.js
+++ components/Warn.js
@@ -5,9 +5,11 @@
 const Warn = (props) => {
   return (
     <div
-      className="bg-orange-100 dark:bg-amber-500 border-s-4 border-orange-500 dark:border-orange-600 font-bold text-orange-500 dark:text-gray-100 px-4 py-6 text-sm mt-40"
+      className="bg-orange-100 dark:bg-amber-500 border-s-4 border-orange-500 dark:border-orange-600 
+                 font-bold text-orange-500 dark:text-gray-100 px-4 pt-4 pb-1 text-sm mt-40"
       role="alert"
     >
+      <span className="text-orange-600 text-lg">تنويه</span>
       {props.children}
     </div>
   )
