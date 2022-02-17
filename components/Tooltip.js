db4097ee9e11c0d9c9e0726e5639aacdc89a4801
hartha-aloufi <hartha.aloufi@gmail.com>
2022-02-18 00:18:36 +0200

    complete the cycle

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -5,15 +5,15 @@ import colors from 'tailwindcss/colors'
 const TermTip = (props) => {
   return (
     <Tooltip
-      content={getTerm(props.t) || props.t}
+      content={getTerm(props.t)?.en || props.t}
       fadeDuration={150}
       background={colors.violet[500]}
       arrow={0}
       padding={4}
       radius={4}
-      border=""
+      border="none"
    >
-      <span className="text-secondary-400 dark:text-secondary-300 cursor-pointer">
+      <span className="text-secondary-500 dark:text-secondary-400 cursor-pointer">
         {props.children}
       </span>
     </Tooltip>
